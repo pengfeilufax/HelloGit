878572f2b437c4cedea721f283059eeaa5aa734c
pengfei_jesse <pengfei_jesse@163.com>
2014-07-19 00:13:28 +0800

    test

--- src/main/java/HelloGit.java
+++
@@ -1,7 +0,0 @@
-package main.java;
-
-/**
- * Created by Administrator on 14-7-14.
- */
-public class HelloGit {
-}
--- src/test/java/HelloGtiTest.java
+++
@@ -1,7 +0,0 @@
-package test.java;
-
-/**
- * Created by Administrator on 14-7-14.
- */
-public class HelloGtiTest {
-}
